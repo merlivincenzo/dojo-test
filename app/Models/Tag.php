73b197f0da9493b878ee5f27f4f12d63f185a865
merlivincenzo <merli.vincenzo@gmail.com>
2020-09-13 16:07:09 +0200

    added SoftDeletes to Order and Tag models

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tag extends Model
 {
+    use SoftDeletes;
+
     public function orders()
     {
         return $this->belongsToMany(Order::class);
