8b7abd050bf0aa987f97ca5093b63ee374d06461
merlivincenzo <merli.vincenzo@gmail.com>
2020-09-13 15:53:28 +0200

    added softdeletes to tags table

--- database/migrations/2019_10_15_091108_create_tags_table.php
+++ database/migrations/2019_10_15_091108_create_tags_table.php
@@ -18,6 +18,7 @@ class CreateTagsTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->timestamps();
+            $table->softDeletes();
         });
 
         factory(Tag::class, 10)->create();
