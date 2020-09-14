5658d590a8525a93eb599add10a89ab96052be7a
merlivincenzo <merlivincenzo@users.noreply.github.com>
2020-09-14 10:14:50 +0200

    create_order_tag migration classname refactored

--- database/migrations/2020_09_13_085043_create_order_tag.php
+++ database/migrations/2020_09_13_085043_create_order_tag.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class OrderTag extends Migration
+class CreateOrderTag extends Migration
 {
     /**
      * Run the migrations.
