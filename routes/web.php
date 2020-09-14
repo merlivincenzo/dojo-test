ad4786d50a31c49c5dae3aaae7e8b1ede176c2d7
merlivincenzo <merlivincenzo@users.noreply.github.com>
2020-09-14 11:47:06 +0200

    added restore route to customers and orders route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('customers/{id}/restore', 'CustomersController@restore')->middleware('auth')->name('customers.restore');
 Route::resource('customers', 'CustomersController')->except('show')->middleware('auth');
+Route::get('orders/{id}/restore', 'OrdersController@restore')->middleware('auth')->name('orders.restore');
 Route::resource('orders', 'OrdersController')->except('show')->middleware('auth');
 Route::get('/home', 'HomeController@index')->name('home');
 
